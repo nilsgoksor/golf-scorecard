02cd72158626285a96456cef54688fd72f5619d2
nilsgoksor <nils.goksor@gmail.com>
2020-04-29 10:04:20 +0200

    HoleSwitcher and HolePage title merged to one

--- src/components/HolePage.js
+++ src/components/HolePage.js
@@ -1,38 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { SET_HOLE } from "../state/actionTypes";
+import React from "react";
 import { useStateValue } from "../state/stateprovider";
-import {
-  TitleContainer,
-  PageTitle,
-} from "../styled-components/styled-components";
-import GolfIcon from "./GolfIcon";
 import HoleSwitcher from "./HoleSwitcher";
 import RoundData from "./RoundData";
 import styled from "styled-components";
 import HoleData from "./HoleData";
 
 const HolePage = ({ history }) => {
+  // eslint-disable-next-line no-unused-vars
   const [{ hole, course, players }, dispatch] = useStateValue();
-  const [nextHole, setNextHole] = useState(hole);
-
-  const changeHole = (input) => {
-    if (input === "" || (parseInt(input) > 0 && parseInt(input) <= 9)) {
-      setNextHole(input);
-    }
-  };
-
-  useEffect(() => {
-    if (
-      parseInt(nextHole) > 0 &&
-      parseInt(nextHole) <= 9 &&
-      nextHole !== hole
-    ) {
-      dispatch({
-        type: SET_HOLE,
-        hole: parseInt(nextHole),
-      });
-    }
-  }, [dispatch, hole, nextHole]);
 
   if (!players || players.length === 0) {
     history.push(`/`);
@@ -40,27 +15,7 @@ const HolePage = ({ history }) => {
 
   return (
     <HolePageContainer>
-      <TitleContainer>
-        <PageTitle>
-          <HoleText
-            value={nextHole}
-            type="number"
-            min="1"
-            max="9"
-            step="1"
-            onChange={(e) => {
-              changeHole(e.target.value);
-            }}
-          ></HoleText>
-        </PageTitle>
-        <GolfIcon />
-      </TitleContainer>
-      <HoleSwitcher
-        hole={hole}
-        holeData={course[hole - 1]}
-        setPreviousHole={() => changeHole(hole - 1)}
-        setNextHole={() => changeHole(hole + 1)}
-      ></HoleSwitcher>
+      <HoleSwitcher holeData={course[hole - 1]}></HoleSwitcher>
       <ContentContainer>
         <HoleData holeData={course[hole - 1]} players={players} hole={hole} />
         <RoundData history={history} />
@@ -70,17 +25,6 @@ const HolePage = ({ history }) => {
 };
 
 export default HolePage;
-
-const HoleText = styled.input`
-  cursor: pointer;
-  background: transparent;
-  color: ${(p) => p.theme.color.white};
-  border: none;
-  font-size: 32px;
-  font-weight: bold;
-  margin-right: -25px;
-  outline: 0;
-`;
 
 const ContentContainer = styled.div`
   display: flex;
--- src/components/HoleSwitcher.js
+++ src/components/HoleSwitcher.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { SET_HOLE } from "../state/actionTypes";
+import { useStateValue } from "../state/stateprovider";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import styled from "styled-components";
@@ -7,30 +9,66 @@ import {
   MediumHeading,
 } from "../styled-components/styled-components";
 
-const HoleSwitcher = ({ hole, holeData, setPreviousHole, setNextHole }) => (
-  <HoleSwitcherContainer>
-    <ArrowContainer
-      onClick={() => {
-        setPreviousHole();
-      }}
-      hide={hole === 1}
-    >
-      <ArrowBackIcon />
-    </ArrowContainer>
-    <HoleNameContainer>
-      <MediumHeading>{holeData.name}</MediumHeading>
-      <SmallText>{`par ${holeData.par} - ${holeData.length} m`}</SmallText>
-    </HoleNameContainer>
-    <ArrowContainer
-      onClick={() => {
-        setNextHole();
-      }}
-      hide={hole === 9}
-    >
-      <ArrowForwardIcon />
-    </ArrowContainer>
-  </HoleSwitcherContainer>
-);
+const HoleSwitcher = ({ holeData }) => {
+  const [{ hole }, dispatch] = useStateValue();
+  const [nextHole, setNextHole] = useState(hole);
+
+  const changeHole = (input) => {
+    if (input === "" || (parseInt(input) > 0 && parseInt(input) <= 9)) {
+      setNextHole(input);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      parseInt(nextHole) > 0 &&
+      parseInt(nextHole) <= 9 &&
+      nextHole !== hole
+    ) {
+      dispatch({
+        type: SET_HOLE,
+        hole: parseInt(nextHole),
+      });
+    }
+  }, [dispatch, hole, nextHole]);
+
+  return (
+    <HoleSwitcherContainer>
+      <ArrowContainer
+        onClick={() => {
+          changeHole(hole - 1);
+        }}
+        hide={hole === 1}
+      >
+        <ArrowBackIcon />
+      </ArrowContainer>
+      <HoleInfoContainer>
+        <HoleNameContainer>
+          <HoleText
+            value={nextHole}
+            type="number"
+            min="1"
+            max="9"
+            step="1"
+            onChange={(e) => {
+              changeHole(e.target.value);
+            }}
+          />
+          <MediumHeading>{holeData.name}</MediumHeading>
+        </HoleNameContainer>
+        <SmallText>{`par ${holeData.par} - ${holeData.length} m`}</SmallText>
+      </HoleInfoContainer>
+      <ArrowContainer
+        onClick={() => {
+          changeHole(hole + 1);
+        }}
+        hide={hole === 9}
+      >
+        <ArrowForwardIcon />
+      </ArrowContainer>
+    </HoleSwitcherContainer>
+  );
+};
 
 export default HoleSwitcher;
 
@@ -40,9 +78,18 @@ const HoleSwitcherContainer = styled.div`
   justify-content: center;
 `;
 
-const HoleNameContainer = styled.div`
+const HoleInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  width: ${(p) => p.theme.width.small};
+`;
+
+const HoleNameContainer = styled.div`
+  display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   padding: 10px;
@@ -53,4 +100,14 @@ const ArrowContainer = styled.div`
   cursor: pointer;
   visibility: visible;
   visibility: ${(p) => p.hide && "hidden"};
+`;
+
+const HoleText = styled.input`
+  cursor: pointer;
+  background: transparent;
+  color: ${(p) => p.theme.color.white};
+  border: none;
+  font-size: 32px;
+  font-weight: bold;
+  outline: 0;
 `;
